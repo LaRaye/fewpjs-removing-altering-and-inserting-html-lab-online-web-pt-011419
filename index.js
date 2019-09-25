14c2d44149a4d259b70bb3e8c022ee117aad9a16
LaRaye Johnson <lnj2105@gmail.com>
2019-09-25 20:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ document.querySelector('main#main').remove();
 
 let newHeader = document.createElement('h1');
 newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'YOUR-NAME is the champion';
