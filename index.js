e9d28bef39dd8dc3bf0fdc58e926e93c89c29653
LaRaye Johnson <lnj2105@gmail.com>
2019-09-25 20:08:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 document.querySelector('main#main').remove();
 
-let newHeader = document.createElement('h1#victory');
-document.body.appendChild(newHeader);
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
